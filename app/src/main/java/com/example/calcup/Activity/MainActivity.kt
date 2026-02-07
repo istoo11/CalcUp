1d7a41d853b54b1be469b7a0b485281d03fc8e02
Istoo11 <romansami40@gmail.com>
2026-02-07 16:47:11 +0100

    Activity main creada

--- app/src/main/java/com/example/calcup/Activity/MainActivity.kt
+++ app/src/main/java/com/example/calcup/Activity/MainActivity.kt
@@ -1,12 +1,15 @@
-package com.example.calcup
+package com.example.calcup.Activity
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.calcup.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
